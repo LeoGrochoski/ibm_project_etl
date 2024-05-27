d5fd39b01dc81fdb1c589527c5528221e82cef75
LeoGrochoski <leogrochoski10@gmail.com>
2024-05-27 16:51:53 -0300

    feat: prosseguindo com criacao do arquivo de etl e ajustando dados

--- etl_code/etl_code.py
+++ etl_code/etl_code.py
@@ -4,16 +4,25 @@ import xml.etree.ElementTree as ET # Biblioteca para manipulação de dados XML
 from datetime import datetime # Biblioteca para trabalhar com data e horario, muito utilizada para geração de logs. 
 
 def extracao_csv(arquivo):
+    """
+    Função para extrair dados dos arquivos .csv
+    """
     dados_csv = pd.read_csv(arquivo)
     return dados_csv
 
 
 def extracao_json(arquivo):
+    """
+    Função para extrair dados dos arquivos .json
+    """
     dados_json = pd.read_json(arquivo, lines=True)
     return dados_json
 
 
 def extracao_xml(arquivo):
+    """
+    Função para extrair dados dos arquivos .json
+    """
     dados_xml = pd.DataFrame(columns=["nome", "email", "telefone", "endereco", "renda"])
     arvore = ET.parse(arquivo)
     raiz = arvore.getroot()
@@ -25,3 +34,31 @@ def extracao_xml(arquivo):
         endereco = registro.find("endereco").text
         renda = float(registro.find("renda").text)
         dados_xml = pd.concat([dados_xml, pd.DataFrame([{"nome":nome, "email":email, "telefone":telefone, "endereco":endereco, "renda":renda}])], ignore_index=True)
+    return dados_xml
+
+# A logica que utilizarei é a criação de um DF vazio para persistir os dados 
+# e depois extrair os dados de cada extensáo e concatena-las no dataframe vazio.
+
+def extracao():
+    """
+    Função para extração dos dados baseado em sua extensão, primeiro criaremos um dataframe para persistir os dados
+    depois processaremos os arquivos respectivos por extensão e por fim retornaremos os dados extraidos.
+    """
+    dados_extraidos = pd.DataFrame(columns=['nome', 'email', 'telefone', 'endereco', 'renda'])
+    
+    # Processando os dados do arquivo csv
+    for arquivo_csv in glob.glob("*.csv"):
+        dados_extraidos = pd.concat([dados_extraidos, pd.DataFrame(extracao_csv(arquivo_csv))], ignore_index=True)
+    
+    # Processando os dados do arquivo json
+    for arquivo_json in glob.glob("*.json"):
+        dados_extraidos = pd.concat([dados_extraidos, pd.DataFrame(extracao_csv(arquivo_json))], ignore_index=True)
+    
+    # Processando os dados do arquivo xml
+    for arquivo_xml in glob.glob("*.xml"):
+        dados_extraidos = pd.concat([dados_extraidos, pd.DataFrame(extracao_csv(arquivo_xml))], ignore_index=True)
+
+    return dados_extraidos
+
+
+# Sugestão de Melhoria: Incluir type hint nas fucões
--- generate_data.py
+++ generate_data.py
@@ -4,10 +4,13 @@ import xml.etree.ElementTree as ET
 from faker import Faker
 from unidecode import unidecode
 
+fake = Faker('pt_BR') # Setei portugues para gerar os dados de clientes
 
-fake = Faker('pt_BR')
-# Função para gerar dados aleatórios usando a lib Faker
 def gerar_dados(num_registros=20):
+    """
+    Esta função tem o objetivo de gerar dados sintéticos para os clientes,
+    estes dados são gerados a partir da biblioteca faker.
+    """
     dados = []
     for _ in range(num_registros):
         dados.append({
@@ -19,37 +22,49 @@ def gerar_dados(num_registros=20):
         })
     return dados
 
-dados = gerar_dados()
+def salvar_csv(dados, nome_arquivo):
+    """
+    Salva os dados em formato CSV.
+    """
+    with open(nome_arquivo, 'w', newline='') as arquivo_csv:
+        campos = ['nome', 'email', 'telefone', 'endereco', 'renda']
+        gerador = csv.DictWriter(arquivo_csv, fieldnames=campos)
+
+        gerador.writeheader()
+        for dado in dados:
+            gerador.writerow(dado)
+
+def salvar_json(dados, nome_arquivo):
+    """
+    Salva os dados em formato JSON.
+    """
+    with open(nome_arquivo, 'w') as arquivo_json:
+        json.dump(dados, arquivo_json, indent=4)
+
+def salvar_xml(dados, nome_arquivo):
+    """
+    Salva os dados em formato XML.
+    """
+    raiz = ET.Element("dados")
+    for dado_x in dados:
+        registro = ET.SubElement(raiz, "registro")
+        for chave, valor in dado_x.items():
+            ET.SubElement(registro, chave).text = str(valor)
+
+    tree = ET.ElementTree(raiz)
+    tree.write(nome_arquivo, encoding="utf-8", xml_declaration=True)
 
 # Criação das variaveis que receberão path para criação dos arquivos
 csv_nome_arquivo = 'C:\Lab\ibm_project_etl\data\dados_clientes.csv'
 json_nome_arquivo = 'C:\Lab\ibm_project_etl\data\dados_clientes.json'
 xml_nome_arquivo = 'C:\Lab\ibm_project_etl\data\clientes.xml'
 
-# Salvamento em CSV
-with open(csv_nome_arquivo, 'w', newline='') as arquivo_csv:
-    campos = ['nome', 'email', 'telefone', 'endereco', 'renda']
-    gerador = csv.DictWriter(arquivo_csv, fieldnames=campos)
+dados = gerar_dados() # Gerando os dados
 
-    gerador.writeheader()
-    for dado in dados:
-        gerador.writerow(dado)
+# Chamando as funções para salvar os arquivos de dados
+salvar_csv(dados, csv_nome_arquivo)
+salvar_json(dados, json_nome_arquivo)
+salvar_xml(dados, xml_nome_arquivo)
 
-# Salvamento em JSON
-with open(json_nome_arquivo, 'w') as arquivo_json:
-    json.dump(dados, arquivo_json, indent=4)
-
-# Salvamento em XML
-raiz = ET.Element("dados")
-
-for dado_x in dados:
-    registro = ET.SubElement(raiz, "registro")
-    for chave, valor in dado_x.items():
-        ET.SubElement(registro, chave).text = str(valor)
-
-tree = ET.ElementTree(raiz)
-tree.write(xml_nome_arquivo, encoding="utf-8", xml_declaration=True)
-
+# Retorna os nomes dos arquivos
 csv_nome_arquivo, json_nome_arquivo, xml_nome_arquivo
-
-# Lembrar de tentar melhorar o codigo incluindo os saves dentro de uma função
